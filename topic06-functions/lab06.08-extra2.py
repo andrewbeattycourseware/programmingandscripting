a746fa1f80c6d8d207627d6b39db2874a2348943
abeatty <andrew.beatty@gmit.ie>
2020-02-17 18:27:15 +0000

    modified lab06 to add extra

---
+++ topic06-functions/lab06.08-extra2.py
@@ -0,0 +1,42 @@
+# a different way or dealing with the users choice
+students= []
+
+
+def displayMenu():
+    print("What would you like to do?")
+    print("\t(a) Add new student")
+    print("\t(v) View students")
+    print("\t(q) Quit")
+    choice = input("Type one letter (a/v/q):").strip()
+    return choice
+
+def doAdd():
+    print("do add")
+
+
+def doView():
+    print("do View")
+
+def doNothing():
+    pass
+
+#the dict that maps a letter to function
+choiceMap = {
+    'a': doAdd,
+    'v': doView,
+    'q': doNothing # q is a valid choice
+
+}
+
+#main program
+choice = displayMenu()
+while(choice != 'q'):
+    if choice in choiceMap:
+        # run the function
+        choiceMap[choice]()
+    else: # use did not enter something valid
+        print("\n\nplease select either a,v or q")
+    
+    choice=displayMenu()
+        
+        
